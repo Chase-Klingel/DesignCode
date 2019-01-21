48e5fd9ba86f48f8044ff79c8c6dd5f915527506
Chase-Klingel <klingelchase@gmail.com>
2019-01-20 19:18:11 -0800

    Add animation to HomeViewController / create SectionViewController

---
+++ DesignCode/SectionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SectionViewController.swift
+//  DesignCode
+//
+//  Created by Chase Klingel on 1/20/19.
+//  Copyright © 2019 Chase Klingel. All rights reserved.
+//
+
+import UIKit
+
+class SectionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
