1a3902a42d7d59ac3d85c33f0abb37c037bdb870
Chase-Klingel <klingelchase@gmail.com>
2019-02-07 18:58:08 -0800

    Add gesture interaction animation to Purchase Scene

---
+++ DesignCode/PurchaseViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PurchaseViewController.swift
+//  DesignCode
+//
+//  Created by Chase Klingel on 2/7/19.
+//  Copyright © 2019 Chase Klingel. All rights reserved.
+//
+
+import UIKit
+
+class PurchaseViewController: UIViewController {
+
+    @IBOutlet var panToClose: InteractionPanToClose!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        panToClose.setGestureRecognizer()
+    }
+    
+}
